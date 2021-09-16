f7fed6524f960065ca2bc48206b11fcc437d2b33
Erik Kiznov <46452865+k1zn@users.noreply.github.com>
2021-09-17 00:29:37 +0500

    v1.0

---
+++ NewGameFix.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         New Game Fix
+// @namespace    https://github.com/k1zn/KlavogonkiAddons
+// @version      1.0
+// @description  Fix for keyboard shortcut " Ctrl + -> "
+// @author       kiZzn
+// @match        *://klavogonki.ru/g/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.addEventListener('keydown', (e) => {
+        if (e.ctrlKey) {
+            let keyCode = e.keyCode || e.which;
+            let gameId = new URLSearchParams(window.location.search).get('gmid')
+            if (keyCode === 0x27 && gameId) location.href = '/g/' + gameId + '.replay';
+        }
+    });
+})();
