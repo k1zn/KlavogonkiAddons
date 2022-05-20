2b5148d539b429d01c9d97e4a1c1f88dd9fb9377
k1zn <46452865+k1zn@users.noreply.github.com>
2022-05-21 00:20:21 +0500

    F*** VK ID

---
+++ VK Auth without VK ID.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         VK Auth without VK ID
+// @version      1.0
+// @description  Authorization in VK without VK ID
+// @author       vk.com/kizzn
+// @match        *://id.vk.com/auth
+// @match        *://id.vk.com/auth*
+// @match        *://oauth.vk.com/authorize?client_id=8172446*
+// @match        *://oauth.vk.com/authorize?client_id=8172446
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+if (location.host === "oauth.vk.com" && !document.title.includes("Вход")) location.href = "https://vk.com";
+else if (location.host === "id.vk.com") location.href = "https://oauth.vk.com/authorize?client_id=8172446&scope=0&redirect_uri=https://vk.com&display=page&response_type=token&revoke=1";
