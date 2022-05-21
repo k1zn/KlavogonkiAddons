6ff11f587bbcdec9a82623d5ceac8ebd9bcf6210
k1zn <46452865+k1zn@users.noreply.github.com>
2022-05-21 19:00:50 +0500

    Update VK Auth without VK ID.user.js

--- VK Auth without VK ID.user.js
+++ VK Auth without VK ID.user.js
@@ -12,7 +12,7 @@
 // ==/UserScript==
 
 if (location.host === "oauth.vk.com" && !document.title.includes("Вход")) {
-    document.documentElement.innerHTML = `<center>Redirecting...</center><script>location.href="https://vk.com/"` // shittycode Yep yes
+    document.documentElement.innerHTML = `<center>Redirecting...</center>` // shittycode Yep yes
     location.href = "https://vk.com";
 }
 else if (location.host === "id.vk.com") location.href = "https://oauth.vk.com/authorize?client_id=8172446&scope=0&redirect_uri=https://vk.com&display=page&response_type=token&revoke=1";
