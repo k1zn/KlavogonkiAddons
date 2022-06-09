2eebd006a597179a6782a543db8508a22dcaa134
k1zn <46452865+k1zn@users.noreply.github.com>
2022-06-09 18:30:55 +0500

    Update. Thanks mysh228

--- VK Auth without VK ID.user.js
+++ VK Auth without VK ID.user.js
@@ -5,14 +5,8 @@
 // @author       vk.com/kizzn
 // @match        *://id.vk.com/auth
 // @match        *://id.vk.com/auth*
-// @match        *://oauth.vk.com/authorize?client_id=8172446*
-// @match        *://oauth.vk.com/authorize?client_id=8172446
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
-if (location.host === "oauth.vk.com" && !document.title.includes("Вход")) {
-    document.documentElement.innerHTML = `<center>Redirecting...</center>` // shittycode Yep yes
-    location.href = "https://vk.com";
-}
-else if (location.host === "id.vk.com") location.href = "https://oauth.vk.com/authorize?client_id=8172446&scope=0&redirect_uri=https://vk.com&display=page&response_type=token&revoke=1";
+location.href = location.protocol + "//vk.com/login?classic_flow=1";
