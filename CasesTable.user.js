083cd787658a0e6f25cd06507cba77558df86d47
Erik Kiznov <46452865+k1zn@users.noreply.github.com>
2022-07-22 22:35:07 +0500

    Init commit

---
+++ CasesTable.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Cases Table
+// @version      1.0
+// @description  Table of cases BUFF -> Steam
+// @author       k1zn
+// @match        *://buff.163.com/table
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // Your code here...
+})();
